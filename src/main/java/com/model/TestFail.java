deb17d744744420642c9ea40d87d84cc29aa23e3
Edward Erik Schmoll <erikschmoll@gmail.com>
2013-10-27 12:08:39 -0200

    Agrego el modelo que utilizara la vista. Edito tambien el gitignore por directorios que me crea el eclipse

---
+++ src/main/java/com/model/TestFail.java
@@ -0,0 +1,20 @@
+package com.model;
+
+public class TestFail {
+
+	String messenger;
+	String name;
+	
+	public TestFail(String name, String messenger){
+		this.messenger = messenger;
+		this.name = name;
+	}
+	
+	public String getMessenger(){
+		return messenger;
+	}
+	
+	public String getName(){
+		return name;
+	}
+}
