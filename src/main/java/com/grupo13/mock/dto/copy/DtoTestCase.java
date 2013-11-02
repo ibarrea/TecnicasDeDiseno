b65fd4a64c5caf6ede64459ed3e9b3257ab3e99a
Edward Erik Schmoll <erikschmoll@gmail.com>
2013-11-02 13:48:41 -0200

    arreglo paquete

---
+++ src/main/java/com/grupo13/mock/dto/copy/DtoTestCase.java
@@ -0,0 +1,17 @@
+package com.grupo13.mock.dto.copy;
+
+import com.grupo13.mock.idto.IDtoTest;
+
+
+public class DtoTestCase implements IDtoTest{
+	String message;
+
+	public DtoTestCase(String message){
+		this.message = message;
+	}
+	
+	@Override
+	public String getMessage(String nameProject) {
+		return "\n\t" + message;
+	}
+}
---
+++ src/main/java/com/grupo13/mock/dto/copy/DtoTestSuite.java
@@ -0,0 +1,40 @@
+package com.grupo13.mock.dto.copy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.grupo13.mock.idto.IDtoTest;
+
+
+
+public class DtoTestSuite implements IDtoTest{
+	
+	String message;
+	List<IDtoTest> lDtoTestCase = new ArrayList<IDtoTest>();
+	
+	public DtoTestSuite(String message){
+		this.message = message;
+	}
+	
+	public void add(IDtoTest iDtoTest){
+		if(!lDtoTestCase.contains(iDtoTest)){
+			lDtoTestCase.add(iDtoTest);
+		}
+	}
+	
+	@Override
+	public String getMessage(String nameProject) {
+		String str = message;
+		String path = message;
+		if(!nameProject.equals("")){
+			str = "\n" + nameProject + "." + message;
+			path = nameProject + "." + message;
+		}
+		for(IDtoTest idtoTest: lDtoTestCase){
+			str += idtoTest.getMessage(path);
+		}
+		return str;
+	}
+
+}
+
